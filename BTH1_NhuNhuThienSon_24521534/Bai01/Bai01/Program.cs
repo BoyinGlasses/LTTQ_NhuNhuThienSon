4f58b4529a22b14cfa1b3d96f37aab61b8f7ec56
Thien Son <nhuson190605@gmail.com>
2025-09-26 14:11:29 +0700

    add

---
+++ BTH1_NhuNhuThienSon_24521534/Bai01/Bai01/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai01
+{
+    internal class Program
+    {
+        static bool IsPrime(int n)
+        {
+            if (n < 2)
+            {
+                return false;
+            }
+            double sqrt = Math.Sqrt(n);
+            for (int i = 2; i <= sqrt; i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap n: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            Random Random = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = Random.Next(int.MinValue, int.MaxValue);
+            }
+            foreach (int i in arr)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (Math.Abs(arr[i] % 2) == 1)
+                {
+                    sum += arr[i];
+                }
+            }
+            Console.WriteLine("Tong cac so le trong mang la: " + sum);
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            { 
+                if (IsPrime(arr[i]))
+                {
+                    count++;
+                }
+            
+            }
+            Console.WriteLine("So so nguyen to trong mang la: " + count);
+            int temp = int.MaxValue;
+            foreach (int i in arr)
+            {
+                int sqrt = (int)Math.Sqrt(i);
+                if(sqrt * sqrt == i && i < temp)
+                {
+                    temp = i;
+                }
+            }
+            if (temp == int.MaxValue)
+            {
+                Console.WriteLine("-1");
+            }
+            else
+            {
+                Console.WriteLine("So chinh phuong nho nhat trong mang la: " + temp);
+            }
+        }
+    }
+}
---
+++ BTH1_NhuNhuThienSon_24521534/Bai03/Bai03/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int day, month, year;
+            Console.Write("Nhap ngay: ");
+            day = int.Parse(Console.ReadLine());
+            Console.Write("Nhap thang: ");
+            month = int.Parse(Console.ReadLine());
+            Console.Write("Nhap nam: ");
+            year = int.Parse(Console.ReadLine());
+            try { 
+                DateTime dateCheck = new DateTime(year, month, day);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ngay thang nam khong hop le!");
+                return;
+            }
+            Console.WriteLine("Ngay thang nam hop le!");
+
+        }
+    }
+}
