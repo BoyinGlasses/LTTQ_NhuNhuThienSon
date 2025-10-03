a511bcd070c57b227ef119aa5c628bb88ab18dd5
Thien Son <nhuson190605@gmail.com>
2025-10-03 14:35:00 +0700

    bo sung them

--- BTH1_NhuNhuThienSon_24521534/Bai06/Bai06/Program.cs
+++ BTH1_NhuNhuThienSon_24521534/Bai06/Bai06/Program.cs
@@ -8,6 +8,15 @@ namespace Bai06
 {
     internal class Program
     {
+        bool isPrime(int n)
+        {
+            if (n < 2) return false;
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0) return false;
+            }
+            return true;
+        }
         static void Main(string[] args)
         {
             int rows;
@@ -55,6 +64,9 @@ namespace Bai06
                     }
                 }
             }
+            Console.WriteLine("Max: " + max);
+            Console.WriteLine("Min: " + min);
+            int d = 0;
             int maxRowSum = int.MinValue;
             for (int i = 0; i < rows; i++)
             {
@@ -66,11 +78,89 @@ namespace Bai06
                 if (rowSum > maxRowSum)
                 {
                     maxRowSum = rowSum;
+                    d = i;
                 }
             }
+            Console.WriteLine("Hang co tong lon nhat la hang " + d + " voi tong la " + maxRowSum);
+            int sumNotPrime = 0;
+            Program p = new Program();
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (!p.isPrime(matrix[i, j]))
+                    {
+                        sumNotPrime += matrix[i, j];
+                    }
+                }
+            }
+            Console.WriteLine("Tong cac so khong phai so nguyen to: " + sumNotPrime);
+            Console.WriteLine("Nhap dong can xoa: ");
+            int k = int.Parse(Console.ReadLine());
+            if (k < 0 || k >= rows)
+            {
+                Console.WriteLine("Chi so dong khong hop le!");
+            }
+            else
+            {
+                int[,] newMatrix = new int[rows - 1, cols];
+                int newRow = 0;
+                for (int i = 0; i < rows; i++)
+                {
+                    if (i == k) continue;
+                    for (int j = 0; j < cols; j++)
+                    {
+                        newMatrix[newRow, j] = matrix[i, j];
+                    }
+                    newRow++;
+                }
+                matrix = newMatrix;
+                rows--;
+                Console.WriteLine("Ma tran sau khi xoa dong " + k + ":");
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < cols; j++)
+                    {
+                        Console.Write(matrix[i, j] + "\t");
+                    }
+                    Console.WriteLine();
+                }
+
+            }
+            int maxColIndex = 0;
+            for (int j = 0; j < cols; j++)
+            {
+                for (int i = 0; i < rows; i++)
+                {
+                    if (matrix[i, j] == max)
+                    {
+                        maxColIndex = j;
+                        break;
+                    }
+                }
+            }
+            int[,] finalMatrix = new int[rows, cols - 1];
+            for (int i = 0; i < rows; i++)
+            {
+                int newCol = 0;
+                for (int j = 0; j < cols; j++)
+                {
+                    if (j == maxColIndex) continue;
+                    finalMatrix[i, newCol] = matrix[i, j];
+                    newCol++;
+                }
+            }
+            Console.WriteLine("Ma tran sau khi xoa cot chua gia tri lon nhat:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols - 1; j++)
+                {
+                    Console.Write(finalMatrix[i, j] + "\t");
+                }
+                Console.WriteLine();
 
 
-
+            }
         }
     }
 }
